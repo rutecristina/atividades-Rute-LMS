621ddda67b796a07426133589cf95acda60da625
rutecristina <rutecristina36.rc@gmai.com>
2018-11-07 10:16:13 -0300

    trabalho 2 - whatsapp

---
+++ whatsapp/grupos.js
@@ -0,0 +1,57 @@
+var Grupos = [
+    {
+        grupo: 'Grupo da família!',
+        id: 'familia',
+        mensagens: [
+            {
+                usuario: 'joao03',
+                texto: 'Tudo bem ?'
+            },
+            {
+                usuario: 'victor23',
+                texto: 'Tudo Tranqs'
+            },
+            {
+                usuario: 'joao03',
+                texto: 'Que bom'
+            },
+        ]
+    },
+    {
+        grupo: 'Churrascão no domingão',
+        id: 'churras',
+        mensagens: [
+            {
+                usuario: 'maria2000',
+                texto: 'Na paz ?'
+            },
+            {
+                usuario: 'victor23',
+                texto: 'Show'
+            },
+            {
+                usuario: 'maria2000',
+                texto: 'Que bom'
+            },
+        ]
+    },
+    {
+        grupo: 'Só topzera',
+        id: 'topzera',
+        mensagens: [
+            {
+                usuario: 'victor03',
+                texto: 'Bom ?'
+            },
+            {
+                usuario: 'robson_alves',
+                texto: 'Bom'
+            },
+            {
+                usuario: 'victor03',
+                texto: 'Que bom'
+            },
+        ]
+    }
+
+]
---
+++ whatsapp/whatsapp.js
@@ -0,0 +1,195 @@
+
+function criarGrupoElemento(nomeGrupo, id) {
+
+    let grupo = document.createElement("div");
+    let img = document.createElement("img");
+    let nome = document.createElement("div");
+
+    grupo.setAttribute("class", "grupo");
+    img.setAttribute("class", "foto");
+    nome.setAttribute("class", "nome");
+    grupo.setAttribute("id", "grupo-" + id);
+    img.setAttribute("src", "perfil.jpg");
+
+    nome.innerText = nomeGrupo;
+
+    grupo.appendChild(img);
+    grupo.appendChild(nome);
+
+    return grupo;
+}
+
+function criarMensagemElemento(usuario, texto) {
+    let msgDiv = document.createElement("div");
+    let tituloDiv = document.createElement("div");
+    let titulo = document.createElement("h3");
+    let textoDiv = document.createElement("div");
+
+    msgDiv.setAttribute("class", "panel panel-default");
+    tituloDiv.setAttribute("class", "panel-heading");
+    titulo.setAttribute("class", "panel-title");
+    textoDiv.setAttribute("class", "panel-body");
+
+    msgDiv.appendChild(tituloDiv);
+    msgDiv.appendChild(textoDiv);
+    tituloDiv.appendChild(titulo);
+
+    titulo.innerText = usuario;
+    textoDiv.innerText = texto;
+
+    return msgDiv;
+}
+
+function configurarGrupos() {
+    let listaGrupos = document.getElementsByClassName("grupo");
+    [...listaGrupos].forEach(function (gp) {
+        let id = gp.getAttribute("id");
+        id = id.replace("grupo-", "");
+        gp.addEventListener("click", function() {
+            window.localStorage.setItem("groupid", id);
+            gerarMensagens(id);
+        });
+    });
+}
+
+function gerarGrupos() {
+
+    let promise = fetch("http://rest.learncode.academy/api/rute/groups");
+    let resultadoJSON = promise.then(res => res.json());
+    resultadoJSON.then(function(grupos) {
+        mostrarGrupos(grupos);
+        configurarGrupos();
+    });
+}
+
+function gerarMensagens(groupId) {
+    console.log(groupId)
+    let promise = fetch("http://rest.learncode.academy/api/rute/" + groupId);
+    let resultadoJSON = promise.then(res => res.json());
+    resultadoJSON.then(msgs => mostrarMensagensGrupo(msgs));
+}
+
+function mostrarGrupos(grupos) {
+    let listaGrupo = document.getElementById("listafriends");
+
+    grupos.forEach(function (el) {
+        let gp = criarGrupoElemento(el.groupName, el.groupId);
+        listaGrupo.appendChild(gp);
+    });
+}
+
+function mostrarMensagensGrupo(mensagens) {
+    console.log(mensagens);
+    let mensagensDiv = document.getElementById("mensagens");
+    mensagensDiv.innerHTML = "";
+    mensagens.forEach(function (msg) {
+        let msgElemento = criarMensagemElemento(msg.userName, msg.message);
+        mensagensDiv.appendChild(msgElemento);
+    });
+}
+
+async function enviarMensagem(event) {
+    event.preventDefault();
+    let campoMsg = document.getElementById("input-msg");
+    let groupid = window.localStorage.getItem("grupoid");
+    let userid = window.localStorage.getItem("userid");
+    let msgUser = {userName: userid, message: campoMsg.value};
+    campoMsg.value = "";
+    let resposta = await fetch("http://rest.learncode.academy/api/rute/" + groupid, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(msgUser)
+    });
+    let msg = await resposta.json();
+    let msgElemento = criarMensagemElemento(userid, msgUser.message);
+    
+    let mensagensDiv = document.getElementById("mensagens");
+    mensagensDiv.appendChild(msgElemento);
+}
+
+function userLogado() {
+    let id = window.localStorage.getItem("userid");
+    return !!id;
+}
+function logar(event) {
+    event.preventDefault();
+    let botao = document.getElementById("entrar");
+    let loginInput = document.getElementById("login-input");
+
+    window.localStorage.setItem("userid", loginInput.value);
+    botao.innerHTML = "Sair";
+    loginInput.value = "";
+    fecharModal();
+}
+function deslogar() {
+    let botao = document.getElementById("entrar");
+    window.localStorage.removeItem("userid");
+    botao.innerHTML = "Entrar";
+}
+function permitir() {
+    if(userLogado()) {
+        deslogar()
+    }
+    else {
+        return mostrarModal();
+    }
+}
+function mostrarModal() {
+    let modalOverlay = document.getElementById("modal-overlay");
+    modalOverlay.classList.add("active");
+}
+function fecharModal() {
+    let modalOverlay = document.getElementById("modal-overlay");
+    modalOverlay.classList.remove("active");
+}
+function configurarModal() {
+    let modalOverlay = document.getElementById("modal-overlay")
+    let close = document.getElementById("close");
+    let formul = document.getElementById("formulario");
+
+    formul.addEventListener("submit", logar);
+
+    close.addEventListener("click", function () {
+        modalOverlay.classList.remove("active");
+    }) 
+
+    window.addEventListener("click", function (event) {
+        if (event.target == modalOverlay)
+            modalOverlay.classList.remove("active");
+    })
+}
+
+async function enviarGp(grupo) {
+    let resposta = await fetch('http://rest.learncode.academy/api/rute/groups', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(grupo)
+    });
+    let resultado = await resposta.json();
+    let grupoElemento = criarGrupoElemento(resultado.groupName, resultado.groupID);
+    document.getElementById("listafriends").appendChild(grupoElemento);
+
+}
+
+function criarGp(event) {
+    let nomeGp = document.getElementById("gruponome");
+    let idGp = document.getElementById("grupoid");
+    if(nomeGp.value && idGp.value) {
+        event.preventDefault();
+        let grupo = {groupName: nomeGp.value, groupID: idGp.value};
+        nomeGp.value = "";
+        idGp.value = "";
+        enviarGp(grupo);
+    }
+}
+
+window.addEventListener("load", function () {
+    gerarGrupos();
+    configurarModal();
+    
+    let botaoLogin = document.getElementById("entrar");
+    botaoLogin.addEventListener("click", permitir);
+    let botaoCriarGrupo = this.document.getElementById("criargrupo-button");
+    botaoCriarGrupo.addEventListener("click", criarGp);
+    document.getElementById("campo-msg").addEventListener("submit", enviarMensagem);
+});
